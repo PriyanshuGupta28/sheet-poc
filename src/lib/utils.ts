eeb59b473c394ced96e79126417c3e36c385017f
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-09-30 18:59:58 +0530

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function throttle<Args extends unknown[]>(
+  fn: (...args: Args) => void,
+  wait = 200
+): (...args: Args) => void {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+  let pendingArgs: Args | null = null;
+
+  return (...args: Args) => {
+    if (timeout) {
+      pendingArgs = args;
+      return;
+    }
+    fn(...args);
+    timeout = setTimeout(() => {
+      timeout = null;
+      if (pendingArgs) {
+        const toRun = pendingArgs;
+        pendingArgs = null;
+        fn(...toRun);
+      }
+    }, wait);
+  };
 }
